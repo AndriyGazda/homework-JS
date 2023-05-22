a55c412128f820cf3b3adc05a9c83be63f048a19
AndriyGazda <gazda.andriy@gmail.com>
2023-05-22 14:29:20 +0300

    Пофіксив задачу з 3 числами

--- lessons1/less1.js
+++ lessons1/less1.js
@@ -289,13 +289,13 @@
 // alert(name + '\n' + age + '\n' + hobby)
 
 //9999999999999999999999999999999999999999999
-let str1 = prompt('Хто ')
-let str2 = prompt('ти ')
-let str3 = prompt('такий ?')
-let concatenation = str1 + str2 + str3;
-
-
-document.write(concatenation);
+// let str1 = prompt('Хто ')
+// let str2 = prompt('ти ')
+// let str3 = prompt('такий ?')
+// let concatenation = str1 + str2 + str3;
+//
+//
+// document.write(concatenation);
 
 
 
@@ -334,22 +334,22 @@ document.write(concatenation);
 
 //.............ДОП............
 
-const num1 = prompt('Число 1 ');
-const num2 = prompt('Число 2 ');
-const num3 = prompt('Число 3 ');
+const num1 = Number(prompt('Число 1 '));
+const num2 = Number(prompt('Число 2 '));
+const num3 = Number(prompt('Число 3 '));
 
 if ((num1 < num2) && (num1 < num3) && (num2 < num3)) {
-    console.log(num1 + num2 + num3);
+    console.log(num1, num2, num3);
 } else if ((num1 < num2) && (num1 < num3) && (num3 < num2)) {
-    console.log(num1 + num3 +num2);
+    console.log(num1, num3, num2);
 } else if ((num2 < num1) && (num2 < num3) && (num1 < num3)) {
-    console.log(num2 + num1 + num3);
+    console.log(num2, num1, num3);
 } else if ((num2 < num1) && (num2 < num3) && (num3 < num1)) {
-    console.log(num2 + num3 + num1);
+    console.log(num2, num3, num1);
 } else if ((num3 < num2) && (num3 < num3) && (num2 < num1)) {
-    console.log(num3 + num2 + num1);
+    console.log(num3, num2, num1);
 } else if ((num3 < num2) && (num3 < num3) && (num1 < num2)) {
-    console.log(num3 + num1 + num2);
+    console.log(num3, num1, num2);
 }
 
 
